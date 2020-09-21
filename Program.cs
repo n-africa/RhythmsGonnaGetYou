23c86f21cccc922eb3f11692ac9b3bd8a4d501bd
Naima Africa Francis <naima.africa@gmail.com>
2020-09-20 21:34:58 -0400

    Added Album and Band class

--- Program.cs
+++ Program.cs
@@ -2,11 +2,30 @@
 
 namespace RhythmsGonnaGetYou
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Album
     {
-      Console.WriteLine("Welcome to C#");
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsExplicit { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public Band BandId { get; set; }
     }
-  }
+    class Band
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CountryOfOrigin { get; set; }
+        public int NumberOfMembers { get; set; }
+        public string Website { get; set; }
+        public string Style { get; set; }
+        public bool IsSigned { get; set; }
+        public string ContactName { get; set; }
+        public string ContactPhoneNumber { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
 }
